8bf34a1845832e59d787dac4f13990a699ede041
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-04-10 15:50:59 +0000

    [update]update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'top' => 'homes#top'
   get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'book/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
